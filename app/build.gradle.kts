51831eccda2c292e6280e95c21b84855f3a208e6
Shugo Maeda <shugo@ruby-lang.org>
2025-10-14 10:48:12 +0900

    Bump version to 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "net.shugo.medicineshield"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "2"
+        versionCode = 3
+        versionName = "3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
