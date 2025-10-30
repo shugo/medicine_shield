a9d7d70e3c0aebb5f806e9d0c126234e0f036c3d
Shugo Maeda <shugo@ruby-lang.org>
2025-10-30 17:15:26 +0900

    Bump version to 8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "net.shugo.medicineshield"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "7"
+        versionCode = 8
+        versionName = "8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
