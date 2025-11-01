1083730345457455da99fd573fb5960bddf9774b
Shugo Maeda <shugo@ruby-lang.org>
2025-11-01 09:13:22 +0900

    Include native symbols in release builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,10 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+
+            ndk {
+                debugSymbolLevel = "FULL"
+            }
         }
     }
 
